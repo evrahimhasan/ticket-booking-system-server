f7cff99478e3ddd385c6d21cac0ff382e1ae50d8
evrahimhasan <evrahimhasanemran.com>
2026-01-28 07:37:05 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -10,10 +10,41 @@ app.use(express());
 
 
 
-app.get('/', (req,res)=>{
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ticket-booking:hOV3ynLY2fv0a8mi@cluster0.83ib2ra.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+        
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Hello, Dev')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log((`Server is running on ${port}`))
 })
