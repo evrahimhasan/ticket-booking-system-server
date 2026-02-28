79d951ab2c70d9805704e36feb0e2e71d352c2e4
evrahimhasan <evrahimhasanemran.com>
2026-02-28 20:26:33 +0600

    added user role, status change, get ticket, delete ticket apis

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ticket-booking:hOV3ynLY2fv0a8mi@cluster0.83ib2ra.mongodb.net/?appName=Cluster0";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.83ib2ra.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,7 @@ async function run() {
 
         const database = client.db('ticket-booking')
         const userCollection = database.collection('users')
+        const TicketCollection = database.collection(('tickets'))
 
         // User
         app.post('/users', async (req, res) => {
@@ -39,15 +40,81 @@ async function run() {
             res.send(result)
         })
 
+        // All User
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            const totalUser = await userCollection.countDocuments()
+            res.send({ user: result, totalUser })
+        })
+
+
+
+        // status change api
+        app.patch('/update/user/status', async (req, res) => {
+            const { email, status } = req.query
+            const query = { email: email }
+
+            const updateStatus = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await userCollection.updateOne(query, updateStatus)
+            res.send(result)
+        })
 
         // User role
         app.get('/users/role/:email', async (req, res) => {
             const { email } = req.params
             const query = { email: email }
             const result = await userCollection.findOne(query)
-            console.log(result);
+            // console.log(result);
             res.send(result)
         })
+
+
+
+
+
+        // Add Tickets
+        app.post('/tickets', async (req, res) => {
+            const data = req.body
+            data.createdAt = new Date()
+            const result = await TicketCollection.insertOne(data)
+            res.send(result)
+        })
+
+
+        // Get All Tickets
+        app.get('/tickets', async (req, res) => {
+            try {
+                const tickets = await TicketCollection
+                    .find()
+                    .sort({ createdAt: -1 })
+                    .toArray();
+
+                res.send(tickets);
+            } catch (error) {
+                res.status(500).send({ message: "Failed to fetch tickets" });
+            }
+        });
+
+
+        // // Update Ticket
+        // app.put('/edit-tickets/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await TicketCollection.updateOne(query);
+        //     res.send(result);
+        // });
+
+        // delete tickets
+        app.delete("/delete-tickets/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await TicketCollection.deleteOne(query);
+            res.send(result);
+        });
 
 
 
